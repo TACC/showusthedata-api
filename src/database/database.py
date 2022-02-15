d0b749c693c2bfcba95aa6ea2b6035ffb9d8f766
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-02-15 16:02:53 -0600

    strip db password

--- src/database/database.py
+++ src/database/database.py
@@ -11,7 +11,7 @@ logger = logging.getLogger()
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD").strip()
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
 logger.debug(
     f"Environment: {POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
